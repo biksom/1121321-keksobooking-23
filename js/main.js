59b93db9233e479f47a610324569582ea419af06
biksom <58607094+biksom@users.noreply.github.com>
2021-06-04 12:52:08 +0300

    Модуль 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+const DEFAULT_PRECISION = 0;
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function getRandomFloat(min, max, precision = DEFAULT_PRECISION) {
+  const minValue = Math.min(min, max);
+  const maxValue = Math.max(min, max);
+  const randomValue = minValue + (Math.random() * (maxValue - minValue));
+  return  Number(randomValue.toFixed(precision));
 }
+
+function getRandomInt(min, max) {
+  return getRandomFloat(min, max);
+}
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
