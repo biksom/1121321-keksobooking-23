38006748fef6a9e35a5dec2b59b69e5860eda807
biksom <58607094+biksom@users.noreply.github.com>
2021-06-13 22:12:53 +0300

    Модуль 4.Переделка

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ function getRandomInt(min, max) {
 }
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-
-
 const AVATARS = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
@@ -41,33 +39,121 @@ const ADRESS = [
   'location.lng'
 ]
 
-const createAuthor = () => {
+const NUMBER = []
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+]
+
+const ROOMS = []
+
+const GUESTS = []
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+]
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+]
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+]
+
+const DESCRIPTION = []
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+]
+
+const LAT = [
+  '35.65000',
+  '35.70000'
+]
+
+const LNG = [
+  '139.70000',
+  '139.80000'
+]
+
+
+const createOffer = () => {
   return {
-    avatar: AVATARS[getRandomFloat(0, 9)]
+    autor: {
+      avatar: AVATARS[getRandomFloat(0, 9)]
+    },
+    title: TITLES[getRandomFloat(0, 4)],
+    address: ADDRES[getRandomFloat(0, 1)],
+    price: NUMBER[getRandomInt()],
+    type: TYPES[getRandomFloat(0, 4)],
+    rooms: ROOMS[getRandomInt()],
+    guests: GUESTS[getRandomInt()],
+    checkin: CHECKIN[getRandomFloat(0, 2)],
+    checkout: CHECKOUT[getRandomFloat(0, 2)],
+    features: FEATURES[getRandomFloat(0, 5)],
+    description: DESCRIPTION[()],
+    photos: PHOTOS[(0, 2)],
+    location: {
+      lat: LAT[getRandomFloat(0, 1)],
+      lng: LNG[getRandomFloat(0, 1)],
+    }
   }
 }
 
 const similarAuthors = new Array(10).fill(null).map(() => createAuthor());
 console.log(similarAuthors);
 
-const createOffer = () => {
-  return {
-    title: TITLES[getRandomFloat(0, 4)]
-  }
-}
+const similarTitles = new Array(5).fill(null).map(() => createTitle());
+console.log(similarTitles);
 
-const similarOffers = new Array(5).fill(null).map(() => createOffer());
-console.log(similarOffers);
+const similarAddres = new Array(2).fill(null).map(() => createAddres());
+console.log(similarAddres);
 
+const similarNumber = new Array().fill(null).map(() => createNumber());
+console.log(similarNumber);
 
-const createOffer = () => {
-  return {
-    address: ADDRES[getRandomFloat(0, 1)]
-  }
-}
+const similarTypes = new Array(5).fill(null).map(() => createTypes());
+console.log(similarTypes);
 
-const similarOffer = new Array(2).fill(null).map(() => createOffer());
-console.log(similarOffers);
+const similarRooms = new Array().fill(null).map(() => createRooms());
+console.log(similarRooms);
+
+const similarGuests = new Array().fill(null).map(() => createGuests());
+console.log(similarGuests);
+
+const similarCheckin = new Array(3).fill(null).map(() => createCheckin());
+console.log(similarCheckin);
+
+const similarCheckout = new Array(3).fill(null).map(() => createCheckout());
+console.log(similarCheckout);
+
+const similarPhotos = new Array(3).fill(null).map(() => createPhotos());
+console.log(similarPhotos);
+
+const similarPhotos = new Array(3).fill(null).map(() => createPhotos());
+console.log(similarPhotos);
+
+const similarLat = new Array(2).fill(null).map(() => createLat());
+console.log(similarLat);
+
+const similarLng = new Array(2).fill(null).map(() => createLng());
+console.log(similarLng);
 
 // const getRandomArrayElement = (elements) => {
 //   return elements[_.random(0, elements.length - 1)];
@@ -111,4 +197,4 @@ console.log(similarOffers);
 //   lat: '',
 //   lng: '',
 
-// }
+// }но н
