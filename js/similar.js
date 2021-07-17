90481f3cc1a27ba5957956813ee7673b42ad9c14
biksom <58607094+biksom@users.noreply.github.com>
2021-07-17 10:53:12 +0300

    Модуль 7, задание 1

---
+++ js/similar.js
@@ -0,0 +1,9 @@
+const card = document.querySelector('.popup__title');
+card.innerText = 'Рай для стариков';
+
+const address = document.querySelector('.popup__text--address');
+card.innerHTML = 'Чебоксары, Набережные Челны, Челябинск';
+
+const price = document.querySelector('.popup__text--price');
+card.innerText = '5200 ₽/ночь';
+
